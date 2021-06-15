a3cfd9f2ed7c092e55e7426b8dfcf3b23696d4e4
xlivings <70450434+xlivings@users.noreply.github.com>
2021-06-15 15:02:46 -0400

    Update proteinReader.py

--- proteinReader.py
+++ proteinReader.py
@@ -1,16 +1,14 @@
-# This py file will read a RNA sequence and determine the amino acids built in the sequence.
+# This py file will read a DNA sequence, then make an RNA sequence that will make the proteins
 # todo try to find a way to figure out what protein is built
 
-class NucleicAcid:
-    def __init__(self, data):
-        self.a = None
-        self.u = None
-        self.c = None
-        self.g = None
-        self.data = data
-
-
-def build_tree(root):
-    acids = ["A", "U", "C", "G"]
-    newRoot = NucleicAcid(root)
-    count = 2
+read = open("sequence.txt")
+sequence = read.read()
+aminoAcids = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+              "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M", "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+              "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+              "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T", "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+              "UAU": "Y", "UAC": "Y", "UAA": "STOP", "UAG": "STOP", "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+              "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K", "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+              "UGU": "C", "UGC": "C", "UGA": "STOP", "UGG": "W", "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+              "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R", "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
+read.close()
